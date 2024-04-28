3cd1cac8199c33f03ba1e7f4a45dc24c11f60743
Kalpesh More <90396306+kalpeshmore1234@users.noreply.github.com>
2024-04-28 15:40:22 +0530

    adding user profile picture

--- src/forms/manage-restaurant-form/DetailsSection.tsx
+++ src/forms/manage-restaurant-form/DetailsSection.tsx
@@ -38,7 +38,7 @@ const DetailsSection = () => {
           control={control}
           name="city"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="flex-1">
               <FormLabel>City</FormLabel>
               <FormControl>
                 <Input {...field} className="bg-white" />
@@ -52,7 +52,7 @@ const DetailsSection = () => {
           control={control}
           name="country"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="flex-1">
               <FormLabel>Country</FormLabel>
               <FormControl>
                 <Input {...field} className="bg-white" />
--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,7 +1,26 @@
-import ManageRestaurantForm from "@/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm";
+import {
+  useCreateMyRestaurant,
+  useGetMyRestaurant,
+  useUpdateMyRestaurant,
+} from "@/api/MyRestaurantApi";
+import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
-     return   <ManageRestaurantForm />;
-}
+  const { createRestaurant, isLoading: isCreateLoading } =
+    useCreateMyRestaurant();
+  const { restaurant } = useGetMyRestaurant();
+  const { updateRestaurant, isLoading: isUpdateLoading } =
+    useUpdateMyRestaurant();
+
+    const isEditing = !!restaurant;
+
+  return (
+    <ManageRestaurantForm
+      restaurant={restaurant}
+      onSave={isEditing ? updateRestaurant: createRestaurant}
+      isLoading={isCreateLoading || isUpdateLoading}
+    />
+  );
+};
 
 export default ManageRestaurantPage;
