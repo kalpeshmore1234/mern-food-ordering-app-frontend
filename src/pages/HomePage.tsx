82a59829a015ff399e926e00f0653424218871fe
Kalpesh More <90396306+kalpeshmore1234@users.noreply.github.com>
2024-04-30 11:56:31 +0530

    added search page and filters

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,32 +1,47 @@
 import landing from "../assets/landing.png";
 import appDownload from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+
   return (
     <div className="flex flex-col gap-12">
-        <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
-            <h1 className="text-5xl font-bold tracking-tight text-orange-600">
-                Tuck into a takeaway today
-            </h1>
+      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+        <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+          Tuck into a takeaway today
+        </h1>
 
-            <span className="text-xl">Food is just a click away!</span>
+        <span className="text-xl">Food is just a click away!</span>
+        <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSearchSubmit}
+        />
+      </div>
+
+      <div className="grid md:grid-cols-2 gap-5">
+        <img src={landing} alt="" />
+
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tighter">
+            Order takeaway even faster!
+          </span>
+
+          <span>
+            Download the MernEats App for faster ordering and personalised
+            recommendations
+          </span>
+
+          <img src={appDownload} />
         </div>
-
-        <div className="grid md:grid-cols-2 gap-5">
-            <img src={landing} alt="" />
-
-            <div className="flex flex-col items-center justify-center gap-4 text-center">
-                <span className="font-bold text-3xl tracking-tighter">
-                     Order takeaway even faster!
-                </span>
-
-                <span>
-                  Download the MernEats App for faster ordering and personalised recommendations
-                </span>
-
-                <img src={appDownload}  />
-            </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
